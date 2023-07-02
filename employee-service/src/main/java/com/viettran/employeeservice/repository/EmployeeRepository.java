0847ba197fd49c2109a8a622a0187ac53c4b1b96
tienviet10 <viettran101294@gmail.com>
2023-07-01 22:54:11 -0400

    Add an employee service

---
+++ employee-service/src/main/java/com/viettran/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,35 @@
+package com.viettran.employeeservice.repository;
+
+import com.viettran.employeeservice.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    private final List<Employee> employees
+            = new ArrayList<>();
+
+    public Employee add(Employee employee) {
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee findById(Long id) {
+        return employees.stream()
+                .filter(a -> a.id().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public List<Employee> findAll() {
+        return employees;
+    }
+
+    public List<Employee> findByDepartment(Long departmentId) {
+        return employees.stream()
+                .filter(a -> a.departmentId().equals(departmentId))
+                .toList();
+    }
+}
